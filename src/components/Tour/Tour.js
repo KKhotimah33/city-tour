b297cca8eb1fd8dcba59f3913f7fec06fd61c8e3
Khusnul Khotimah <kkhotimah33@gmail.com>
2020-01-28 11:52:11 +0700

    finish project

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -2,24 +2,34 @@ import React, {Component} from 'react';
 import './Tour.scss';
 
 export default class Tour extends Component{
+	state={
+		showInfo: false
+	};
+	handleInfo = ()=>{
+		this.setState({
+			showInfo: !this.state.showInfo
+		});
+	}
 	render(){
+		const{id,city,img,name,info}= this.props.tour;
+		const {removeTour} = this.props;
 		return <article className="tour">
 			<div className="img-container">
-				<img src="https://images.pexels.com/photos/3551207/pexels-photo-3551207.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="foto da"/>
-				<span className="close-btn">
+				<img src={img} alt="gambar tidak muncul"/>
+				<span className="close-btn" onClick={()=>{removeTour(id);}}>
 					<i className="fas fa-window-close" />
 				</span>
 			</div>
 			<div className="tour-info">
-				<h3>City</h3>
-				<h4>Name</h4>
+				<h3>{city}</h3>
+				<h4>{name}</h4>
 				<h5>info{" "}
-					<span>
+					<span onClick={this.handleInfo}>
 						<i className="fas fa-caret-square-down"/>
 					</span>
-				</h5>
-				<p> lorem ipsum
-				</p>
+				</h5>{this.state.showInfo && 
+				<p> {info}
+				</p>}
 			</div>
 		</article>;
 	}
---
+++ src/components/tourData.js
@@ -0,0 +1,30 @@
+export const tourData = [
+	{
+		id: 1,
+		city: "Jakarta",
+		img: "./img/kotu.jpg",
+		name: "Kota Tua Jakarta",
+		info: "detail kota tua"
+	},
+	{
+		id: 2,
+		city: "Bandung",
+		img: "./img/bandung.jpg",
+		name: "Dusun Bambu",
+		info: "detail Dusun Bambu"
+	},
+	{
+		id: 3,
+		city: "Yogyakarta",
+		img: "./img/jogja.jpg",
+		name: "Malioboro",
+		info: "detail wisata Malioboro"
+	},
+	{
+		id: 4,
+		city: "Malang",
+		img: "./img/malang.jpg",
+		name: "Pantai Bale Kambang",
+		info: "detail wisata pantai bale Kambang"
+	}
+]
